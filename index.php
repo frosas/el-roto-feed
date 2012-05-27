07d6586da1389da795436db364dc23700f90dc69
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-05-27 09:16:19 +0200

    Remove App

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
-use Frosas\ElRoto\App;
+use Silex\Application;
+use Frosas\ElRoto\Feed;
+use Frosas\ElRoto\Strips;
+use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$app = new App;
+$app = new Application;
+
 $app['debug'] = true;
+
+$app->get('feed', function(Application $app) {
+    $feed = new Feed(new Strips);
+    return new Response($feed->build(), 200, array(
+        'Content-Type' => 'text/xml' // application/rss+xml'
+    ));
+});
+
 $app->run();
